5de39c82ad26ceca91cd9e15ef6ae449fccdc657
Luke Bechtel <lukebechtel4@gmail.com>
2015-12-03 22:29:53 -0500

    fix c++ leftovers

--- example3.cu
+++ example3.cu
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 __global__
-void saxpy(int n, float a, float *x, float *y)
+void saxpy(int n, int a, int *x, int *y)
 {
   int i = blockIdx.x*blockDim.x + threadIdx.x;
   
-  y[i] = (float*)i%n;
+  y[i] = i%n;
 
   // f (i%n != 0) {
   //   y[i] = 1;
@@ -18,20 +18,21 @@ void saxpy(int n, float a, float *x, float *y)
 int main(void)
 {
   int N = 1<<20;
-  float *x, *y, *d_x, *d_y;
-  x = (float*)malloc(N*sizeof(float));
-  y = (float*)malloc(N*sizeof(float));
+  int sum;
+  int *x, *y, *d_x, *d_y;
+  x = (int*)malloc(N*sizeof(int));
+  y = (int*)malloc(N*sizeof(int));
 
-  cudaMalloc(&d_x, N*sizeof(float)); 
-  cudaMalloc(&d_y, N*sizeof(float));
+  cudaMalloc(&d_x, N*sizeof(int)); 
+  cudaMalloc(&d_y, N*sizeof(int));
 
   for (int i = 0; i < N; i++) {
-    x[i] = 1.0f;
-    y[i] = 2.0f;
+    x[i] = 1;
+    y[i] = 2;
   }
 
-  cudaMemcpy(d_x, x, N*sizeof(float), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_y, y, N*sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_x, x, N*sizeof(int), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_y, y, N*sizeof(int), cudaMemcpyHostToDevice);
 
   for(i=0; i < n; i ++)
   {
@@ -39,7 +40,7 @@ int main(void)
 
     // Perform SAXPY on 1M elements
     saxpy<<<(N+255)/256, 256>>>(i, 2.0f, d_x, d_y);
-    cudaMemcpy(y, d_y, N*sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(y, d_y, N*sizeof(int), cudaMemcpyDeviceToHost);
 
     // float maxError = 0.0f;
     // for (int i = 0; i < N; i++)
@@ -60,5 +61,5 @@ int main(void)
       sum = sum + i;
     }
   }
-  cout << sum << endl;
+  //cout << sum << endl;
 }
